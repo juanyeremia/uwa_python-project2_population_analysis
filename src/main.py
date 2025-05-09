1f8b60854a1d66dc6e594788530f18a1b1e9fedf
Juan Yeremia Yovian <juan.yovian@gmail.com>
2025-05-09 13:03:55 +0800

    Completed duplicate SA2 code detection and removal

--- src/main.py
+++ src/main.py
@@ -13,7 +13,23 @@ def read_csv(csvfile):
 # 1.2. Data cleaning
 # 1.2.1 Remove duplicates
 # Get list of duplicates in a CSV file for cross referencing
+def find_dup(list):
+    dup = []
+    for i in range(len(list)-1): 
+        if list[i][0] == list[i+1][0]:
+            dup.append(list[i][0])
+    
+    # No need to check last row because it will not be a duplicate
+    return dup
 
+# Check a data list with another file's duplicate list
+def cleanup(csvdata,duplicates):
+    cleaned_data = []
+    for row in csvdata:
+       if row[0] not in duplicates:
+           cleaned_data.append(row)
+    
+    return cleaned_data
 
 # ----------------------------------------------------------------------
 
@@ -22,10 +38,11 @@ def main(csvfile_1,csvfile_2):
     # Get csv data, sort, and clean
     csv1_data = read_csv(csvfile_1)
     csv1_header = csv1_data[0]
-    csv1_data = cev1_data[1:]
+    csv1_data = csv1_data[1:]
     csv1_data.sort(key=lambda x:x[0])
+    
     
     csv2_data = read_csv(csvfile_2)
     csv2_header = csv2_data[0]
-    csv2_data = cev2_data[1:]
+    csv2_data = csv2_data[1:]
     csv2_data.sort(key=lambda x:x[0])
